8d32309d98d1466e27ec967d6bc151d5db028115
Sam Curren <telegramsam@gmail.com>
2020-04-01 08:19:53 -0600

    Repo Migration Commit
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,67 @@
+# Changelog
+
+All notable changes to this project will be documented in this file
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+- Trust ping support
+- Protocol discovery support
+- App center prebuild script for injecting secrets
+- App center insights and crash reporting
+- Auto refresh connections list on new connection
+- Delete connection support
+- Decoding invitiations from a valid URL format
+- Return routing support for connection establishment
+- Refactored app from poc to osma
+- README section on project structure
+- README section on Gorilla Player UI Development
+- README section on GIT LFS
+- Streetcred.Sdk binaries set up with Git Large File Storage (LFS)
+- This CHANGELOG  with inital additions
+- Register new wallet view
+- Create a new wallet
+- Main page Tab view
+- Connections page navigation view
+- Connections page display current connections
+- ConnectionsPartialView displaying a single connection 
+- Base style resource dictionary
+- Gorrila player support
+- Inverse Boolean Converter
+- Navigation Service
+- Services module peer project
+- Design time data for Main Page
+- Design time data for Connections Page
+- Design time data for ConenctionPartialView
+- Default visual studio .gitignore
+- Updated gitignore to ignore Android Resource.designer.cs
+- Image rendering support via Xamarin.FFImageLoading.Forms
+- QR code scanning support via ZXing.Net.Mobile.Forms
+- Added android camera permission
+- Added baseline button styles
+- Add the ability to navigate to pages without a bound VM to the navigation service
+- AcceptInvite Page with design time data
+- AcceptInviteViewModel with required functionality to interact with connections and invites
+- Exposed the required properties to render connections in the ConnectionsViewModel
+- Bound connections to connection records from the wallet
+- Add button on connections toolbar
+- Connections transaction templates
+- Connections template selector
+- Connection/s icons
+- Add vimm swp files to .gitignore
+- Credentials Page
+- Credential Page
+- Load example credential in DEBUG
+- Register Services module iOS AppDelegate
+- Create Detailed Cell component
+- Add Xamarin.Essentials package
+- Add icons for accoount page
+- Create Legal page
+- Create Account page
+- Create AccountMenuButtonView
+
+[Unreleased]: https://bitbucket.org/sparkchain/poc-mobile-app/compare/HEAD..13ad5882e4c9a526a27cde19270bcbc56c1d53fa
---
+++ src/Osma.Mobile.App/ViewModels/Connections/ConnectionViewModel.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using AgentFramework.Core.Contracts;
+using AgentFramework.Core.Messages;
+using AgentFramework.Core.Messages.Common;
+using AgentFramework.Core.Messages.Discovery;
+using AgentFramework.Core.Models.Records;
+using Osma.Mobile.App.Events;
+using Osma.Mobile.App.Extensions;
+using Osma.Mobile.App.Services.Interfaces;
+using Osma.Mobile.App.Views.Connections;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace Osma.Mobile.App.ViewModels.Connections
+{
+    public class ConnectionViewModel : ABaseViewModel
+    {
+        private readonly ConnectionRecord _record;
+
+        private readonly ICustomAgentContextProvider _agentContextProvider;
+        private readonly IMessageService _messageService;
+        private readonly IDiscoveryService _discoveryService;
+        private readonly IConnectionService _connectionService;
+        private readonly IEventAggregator _eventAggregator;
+
+        public ConnectionViewModel(IUserDialogs userDialogs,
+                                   INavigationService navigationService,
+                                   ICustomAgentContextProvider agentContextProvider,
+                                   IMessageService messageService,
+                                   IDiscoveryService discoveryService,
+                                   IConnectionService connectionService,
+                                   IEventAggregator eventAggregator,
+                                   ConnectionRecord record) :
+                                   base(nameof(ConnectionViewModel),
+                                       userDialogs,
+                                       navigationService)
+        {
+            _agentContextProvider = agentContextProvider;
+            _messageService = messageService;
+            _discoveryService = discoveryService;
+            _connectionService = connectionService;
+            _eventAggregator = eventAggregator;
+
+            _record = record;
+            MyDid = _record.MyDid;
+            TheirDid = _record.TheirDid;
+            ConnectionName = _record.Alias.Name;
+            ConnectionSubtitle = $"{_record.State:G}";
+            ConnectionImageUrl = _record.Alias.ImageUrl;
+        }
+
+        public override async Task InitializeAsync(object navigationData)
+        {
+            await RefreshTransactions();
+            await base.InitializeAsync(navigationData);
+        }
+
+        public async Task RefreshTransactions()
+        {
+            RefreshingTransactions = true;
+
+            var context = await _agentContextProvider.GetContextAsync();
+            var message = _discoveryService.CreateQuery(context, "*");
+
+            DiscoveryDiscloseMessage protocols = null;
+
+            try
+            {
+                var response = await _messageService.SendAsync(context.Wallet, message, _record, null, true);
+                protocols = response.GetMessage<DiscoveryDiscloseMessage>();
+            }
+            catch (Exception)
+            {
+                //Swallow exception
+                //TODO more granular error protection
+            }
+
+            IList<TransactionItem> transactions = new List<TransactionItem>();
+
+            Transactions.Clear();
+
+            if (protocols == null)
+            {
+                HasTransactions = false;
+                RefreshingTransactions = false;
+                return;
+            }
+
+            foreach (var protocol in protocols.Protocols)
+            {
+                switch (protocol.ProtocolId)
+                {
+                    case MessageTypes.TrustPingMessageType:
+                        transactions.Add(new TransactionItem()
+                        {
+                            Title = "Trust Ping",
+                            Subtitle = "Version 1.0",
+                            PrimaryActionTitle = "Ping",
+                            PrimaryActionCommand = new Command(async () =>
+                            {
+                                await PingConnectionAsync();
+                            }, () => true),
+                            Type = TransactionItemType.Action.ToString("G")
+                        });
+                        break;
+                }
+            }
+
+            Transactions.InsertRange(transactions);
+            HasTransactions = transactions.Any();
+
+            RefreshingTransactions = false;
+        }
+
+        public async Task PingConnectionAsync()
+        {
+            var dialog = UserDialogs.Instance.Loading("Pinging");
+            var context = await _agentContextProvider.GetContextAsync();
+            var message = new TrustPingMessage
+            {
+                ResponseRequested = true
+            };
+
+            bool success = false;
+            try
+            {
+                var response = await _messageService.SendAsync(context.Wallet, message, _record, null, true);
+                var trustPingResponse = response.GetMessage<TrustPingResponseMessage>();
+                success = true;
+            }
+            catch (Exception)
+            {
+                //Swallow exception
+                //TODO more granular error protection
+            }
+
+            if (dialog.IsShowing)
+            {
+                dialog.Hide();
+                dialog.Dispose();
+            }
+
+            DialogService.Alert(
+                    success ?
+                    "Ping Response Recieved" :
+                    "No Ping Response Recieved"
+                );
+        }
+
+        #region Bindable Command
+        public ICommand NavigateBackCommand => new Command(async () =>
+        {
+            await NavigationService.NavigateBackAsync();
+        });
+
+        public ICommand DeleteConnectionCommand => new Command(async () =>
+        {
+            var dialog = DialogService.Loading("Deleting");
+
+            var context = await _agentContextProvider.GetContextAsync();
+            await _connectionService.DeleteAsync(context, _record.Id);
+
+            _eventAggregator.Publish(new ApplicationEvent() { Type = ApplicationEventType.ConnectionsUpdated });
+
+            if (dialog.IsShowing)
+            {
+                dialog.Hide();
+                dialog.Dispose();
+            }
+
+            await NavigationService.NavigateBackAsync();
+        });
+
+        public ICommand RefreshTransactionsCommand => new Command(async () => await RefreshTransactions());
+        #endregion
+
+        #region Bindable Properties
+        private string _connectionName;
+        public string ConnectionName
+        {
+            get => _connectionName;
+            set => this.RaiseAndSetIfChanged(ref _connectionName, value);
+        }
+
+        private string _myDid;
+        public string MyDid
+        {
+            get => _myDid;
+            set => this.RaiseAndSetIfChanged(ref _myDid, value);
+        }
+
+        private string _theirDid;
+        public string TheirDid
+        {
+            get => _theirDid;
+            set => this.RaiseAndSetIfChanged(ref _theirDid, value);
+        }
+
+        private string _connectionImageUrl;
+        public string ConnectionImageUrl
+        {
+            get => _connectionImageUrl;
+            set => this.RaiseAndSetIfChanged(ref _connectionImageUrl, value);
+        }
+
+        private string _connectionSubtitle = "Lorem ipsum dolor sit amet";
+        public string ConnectionSubtitle
+        {
+            get => _connectionSubtitle;
+            set => this.RaiseAndSetIfChanged(ref _connectionSubtitle, value);
+        }
+
+        private RangeEnabledObservableCollection<TransactionItem> _transactions = new RangeEnabledObservableCollection<TransactionItem>();
+        public RangeEnabledObservableCollection<TransactionItem> Transactions
+        {
+            get => _transactions;
+            set => this.RaiseAndSetIfChanged(ref _transactions, value);
+        }
+
+        private bool _refreshingTransactions;
+        public bool RefreshingTransactions
+        {
+            get => _refreshingTransactions;
+            set => this.RaiseAndSetIfChanged(ref _refreshingTransactions, value);
+        }
+
+        private bool _hasTransactions;
+        public bool HasTransactions
+        {
+            get => _hasTransactions;
+            set => this.RaiseAndSetIfChanged(ref _hasTransactions, value);
+        }
+        #endregion
+    }
+}
